411c8908d4e85742376ff1f8f53e272c13e35a37
dmsskgus@naver.com <dmsskgus@naver.com>
2025-04-14 17:38:06 +0900

    feat: first commit

---
+++ src/main/java/org/example/burtyserver/BurtyServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.burtyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurtyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurtyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/burtyserver/BurtyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.burtyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurtyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

2eb0b820068080e7e31006a859debf0ff7bb9e06
dmsskgus@naver.com <dmsskgus@naver.com>
2025-04-19 14:43:22 +0900

    feat: Refresh token Entity 추가

--- src/main/java/org/example/burtyserver/domain/auth/model/entity/RefreshToken.java
+++ src/main/java/org/example/burtyserver/domain/auth/model/entity/RefreshToken.java
@@ -1,4 +1,43 @@
 package org.example.burtyserver.domain.auth.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.burtyserver.domain.user.model.entity.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+
+    public void revoke() {
+        this.revoked = true;
+    }
 }

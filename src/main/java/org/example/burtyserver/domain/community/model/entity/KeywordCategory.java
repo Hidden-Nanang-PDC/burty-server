96a5a7f4a281265e62b8653a2274d9c10694db9b
dmsskgus@naver.com <dmsskgus@naver.com>
2025-04-23 11:51:33 +0900

    feat: 키워드, 키워드 카테고리 Entity 생성

---
+++ src/main/java/org/example/burtyserver/domain/community/model/entity/KeywordCategory.java
@@ -0,0 +1,28 @@
+package org.example.burtyserver.domain.community.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "community_keyword_categories")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KeywordCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name; // 지역, 연령대, 직무
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Keyword> keywords = new ArrayList<>();
+}
